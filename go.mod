11a09e9b60f62142ae528010eae63d7d7746f04a
Alaas <alaa@bifolia.ma>
2024-07-19 23:56:11 +0100

    Add package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module timeiso
+
+go 1.22.3
+
+require github.com/google/martian/v3 v3.3.3
---
+++ timeiso.go
@@ -0,0 +1,15 @@
+package timeiso
+
+import (
+	"errors"
+	"time"
+)
+
+func dateToTime(date string) (string, error) {
+	dateFrom, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return "", errors.New("Date should be in format 2006-01-02")
+	}
+	dateEventFrom := dateFrom.UTC().Format(time.RFC3339)
+	return dateEventFrom, nil
+}
