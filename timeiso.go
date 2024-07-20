eab6438d4e99903e5183828671941c1cdf296fc8
Alaas <alaa@bifolia.ma>
2024-07-20 01:22:39 +0100

    parse string format 2006-01-02 to time

--- timeiso.go
+++ timeiso.go
@@ -5,11 +5,10 @@ import (
 	"time"
 )
 
-func DateToTime(date string) (string, error) {
+func DateToTime(date string) (*time.Time, error) {
 	dateFrom, err := time.Parse("2006-01-02", date)
 	if err != nil {
-		return "", errors.New("Date should be in format 2006-01-02")
+		return nil, errors.New("Date should be in format 2006-01-02")
 	}
-	dateEventFrom := dateFrom.UTC().Format(time.RFC3339)
-	return dateEventFrom, nil
+	return &dateFrom, nil
 }
